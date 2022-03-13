b7953d82f2faea2d7e743625340b5c9b2d8fab60
erkan <elanpir@gmail.com>
2022-03-13 17:12:22 +0300

    Topun kinematic özelliklerindeki yanlış davranış düzenlendi. Commentler eklendi.

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -19,34 +19,34 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(currentBallRigidBody == null) {return; }
-        if(!Touchscreen.current.primaryTouch.press.isPressed)
+        if(currentBallRigidBody == null) {return; }  //frame tamamlanınca rigidbody null ise bişey yapma.
+        if(!Touchscreen.current.primaryTouch.press.isPressed) //Ekrana dokunulmayan frame ler için yapılacaklar. Top bırakıldığında buraya girer.
         {
-            if (isDragging)
+            if (isDragging) // İlk dokunma sonrası top bırakıldığı anda isPressed false isDragging true olacak ve LaunchBall çağırılacak.
             {
-                LaunchBall();
+                LaunchBall(); 
             }
             isDragging = false;
            // currentBallRigidBody.isKinematic = false;
             return;
         }
         isDragging=true;
-        //currentBallRigidBody.isKinematic = true;
+        currentBallRigidBody.isKinematic = true;
         Vector2 touchpos = Touchscreen.current.primaryTouch.position.ReadValue();
 
-        Vector3 worldpos = mainCamera.ScreenToWorldPoint(touchpos);
+        Vector3 worldpos = mainCamera.ScreenToWorldPoint(touchpos); //Touch position ekran pixelinden oyun dünyası noktalarına (x,y,z) çevrildi. 
         currentBallRigidBody.position = worldpos;        
         //Debug.Log(worldpos);
         
     }
-    private void LaunchBall()
+    private void LaunchBall() //Top çekildikten sonra bırakıldığında yapılacaklar
     {
-        currentBallRigidBody.isKinematic = true;
+        currentBallRigidBody.isKinematic = false;
         currentBallRigidBody = null;
-        Invoke(nameof(DetachBall),delayDuration);
+        Invoke(nameof(DetachBall),delayDuration); //delay kadar süre fark oluşturarak sprintjointin hareketten sonra disable edilmesi sağlandı.  
 
     }
-    private void DetachBall()
+    private void DetachBall() //Topun bağlı olduğu SprintJoint i çözerek topu özgür bırakma
     {
         currentBallSpringJoint.enabled=false;
         currentBallSpringJoint = null;
